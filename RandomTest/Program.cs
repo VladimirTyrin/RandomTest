94721324f3d12cb0bb0e784f1d4d302aef1c3dd5
Vladimir Tyrin <b0-0b@yandex.ru>
2017-06-06 16:51:04 +0300

    test added

--- RandomTest/Program.cs
+++ RandomTest/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
 
 namespace RandomTest
 {
-	class Program
+	internal class Program
 	{
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
+			while (true)
+			{
+				var currentValue = -1;
+				var counter = 0;
+				var sw = Stopwatch.StartNew();
+				
+				while (true)
+				{
+					var random = new Random();
+					var next = random.Next(1, 100);
+
+					if (currentValue < 0)
+					{
+						currentValue = next;
+						continue;
+					}
+					if (next != currentValue)
+						break;
+
+					counter++;
+				}
+				Console.WriteLine($"{counter} duplicates ({(double) sw.ElapsedTicks * 1000 / Stopwatch.Frequency} ms)");
+				Thread.Sleep(500);
+			}
 		}
 	}
 }
